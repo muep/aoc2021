85f7d57542a16cb01bd8943540a1a233a338b5b5
Joonas Sarajärvi <muep@iki.fi>
2021-12-01 19:59:02 +0200

    Implement part 1 of day 1

--- src/bin/day-1.rs
+++ src/bin/day-1.rs
@@ -1,3 +1,28 @@
+use std::env::args;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn run(file_path: &str) {
+    let file = File::open(file_path).unwrap();
+
+    let mut lines = BufReader::new(file).lines().map(|x| x.unwrap());
+    let mut prev = lines.next().unwrap().parse::<u32>().unwrap();
+    let mut cnt = 0;
+
+    for line in lines {
+        let num = line.parse::<u32>().unwrap();
+        if num > prev {
+            cnt += 1;
+        }
+        prev = num;
+    }
+
+    println!("{}", cnt);
+}
+
 fn main() {
-    println!("Hello, World!");
+    for arg in args().skip(1) {
+        run(&arg);
+    }
 }
