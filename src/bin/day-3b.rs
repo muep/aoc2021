cf22a61bcb96e8758cbbd72f850d54949d803a01
Joonas Sarajärvi <muep@iki.fi>
2021-12-04 09:39:08 +0200

    Add day-3b

---
+++ src/bin/day-3b.rs
@@ -0,0 +1,123 @@
+use std::io::stdin;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::Read;
+
+fn bits_in_use(v: &u16) -> u8 {
+    for n in 0..16 {
+        if *v < (1 << n) {
+            return n;
+        }
+    }
+    return 16;
+}
+
+fn bit_at_pos(bit_pos: u8, num: u16) -> bool {
+    (num >> bit_pos) & 1 != 0
+}
+
+fn most_common_at_pos(bit_pos: u8, nums: &[u16]) -> bool {
+    let one_cnt = nums
+        .iter()
+        .filter(|n| bit_at_pos(bit_pos, **n) == true)
+        .count();
+    let zero_cnt = nums.len() - one_cnt;
+
+    if zero_cnt <= one_cnt {
+        true
+    } else  {
+        false
+    }
+}
+
+fn find_ogr(input: Vec<u16>, bits: u8) -> u16 {
+    let mut remaining = input;
+
+    for b in (0..bits).rev() {
+        let most_common_at_b = most_common_at_pos(b, &remaining);
+
+        remaining = remaining
+            .into_iter()
+            .filter(|n| bit_at_pos(b, *n) == most_common_at_b)
+            .collect();
+
+        if remaining.len() <= 1 {
+            break;
+        }
+    }
+    return *remaining.first().unwrap();
+}
+
+fn find_csr(input: Vec<u16>, bits: u8) -> u16 {
+    let mut remaining = input;
+
+    for b in (0..bits).rev() {
+        let least_common_at_b = !most_common_at_pos(b, &remaining);
+
+        remaining = remaining
+            .into_iter()
+            .filter(|n| bit_at_pos(b, *n) == least_common_at_b)
+            .collect();
+
+        if remaining.len() <= 1 {
+            break;
+        }
+    }
+    return *remaining.first().unwrap();
+}
+
+fn find_ogr_csr(input: &mut dyn Read) -> (u32, u32) {
+    let numbers: Vec<u16> = BufReader::new(input)
+        .lines()
+        .map(|l| u16::from_str_radix(&l.unwrap(), 2).unwrap())
+        .collect();
+
+    let bits = numbers.iter().map(bits_in_use).max().unwrap();
+    let ogr = find_ogr(numbers.clone(), bits);
+    let csr = find_csr(numbers.clone(), bits);
+
+    (ogr as u32, csr as u32)
+}
+
+fn main() {
+    let mut input = stdin();
+    let (ogr, csr) = find_ogr_csr(&mut input);
+    println!("{} * {} -> {}", ogr, csr, ogr * csr);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bit_at_pos() {
+        assert_eq!(bit_at_pos(0, 1), true);
+        assert_eq!(bit_at_pos(1, 1), false);
+
+        assert_eq!(bit_at_pos(0, 5), true);
+        assert_eq!(bit_at_pos(1, 5), false);
+        assert_eq!(bit_at_pos(2, 5), true);
+    }
+
+    #[test]
+    fn test_bits_in_use() {
+        assert_eq!(bits_in_use(&1), 1);
+        assert_eq!(bits_in_use(&3), 2);
+        assert_eq!(bits_in_use(&4), 3);
+        assert_eq!(bits_in_use(&31), 5);
+        assert_eq!(bits_in_use(&32), 6);
+        assert_eq!(bits_in_use(&4095), 12);
+        assert_eq!(bits_in_use(&4096), 13);
+    }
+
+    #[test]
+    fn test_example() {
+        use std::fs::File;
+        let mut f = File::open("input/day-3-sample.txt").unwrap();
+        let (ogr, csr) = find_ogr_csr(&mut f);
+
+        assert_eq!(ogr, 23);
+        assert_eq!(csr, 10);
+        assert_eq!(ogr * csr, 230);
+    }
+}
