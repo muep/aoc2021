a1a6801758375f51f695d75d4c18597c817b1510
Joonas Sarajärvi <muep@iki.fi>
2021-12-11 21:49:58 +0200

    Day 10 part 1

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,7 +1,59 @@
 use std::io::Read;
 
-fn part1(_: &mut dyn Read) -> u32 {
-    26397
+enum Paren {
+    Opening(char),
+    Closing(char),
+}
+
+impl Paren {
+    fn from_char(c: char) -> Paren {
+        match c {
+            '(' => Paren::Opening('('),
+            '[' => Paren::Opening('['),
+            '{' => Paren::Opening('{'),
+            '<' => Paren::Opening('<'),
+            ')' => Paren::Closing('('),
+            ']' => Paren::Closing('['),
+            '}' => Paren::Closing('{'),
+            '>' => Paren::Closing('<'),
+            _ => panic!("unexpected char {}", c),
+        }
+    }
+}
+
+fn part1_line(line: &str) -> Option<u32> {
+    let mut stack = Vec::new();
+
+    for c in line.chars() {
+        match Paren::from_char(c) {
+            Paren::Opening(c) => {
+                stack.push(c);
+            }
+            Paren::Closing(c) => {
+                let expected = stack.pop().unwrap();
+                if expected != c {
+                    return Some(match c {
+                        '(' => 3,
+                        '[' => 57,
+                        '{' => 1197,
+                        '<' => 25137,
+                        _ => panic!(),
+                    });
+                }
+            }
+        }
+    }
+    None
+}
+
+fn part1(input: &mut dyn Read) -> u32 {
+    use std::io::{BufRead, BufReader};
+
+    BufReader::new(input)
+        .lines()
+        .map(|l| part1_line(&l.unwrap()))
+        .filter_map(|a| a)
+        .sum()
 }
 
 pub fn run_part1(input: &mut dyn Read) {
