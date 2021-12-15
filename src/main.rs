0a9b79cab5f7def67cef48340ff26e1af05fc942
Joonas Sarajärvi <muep@iki.fi>
2021-12-15 18:22:07 +0200

    Add day 14 stub

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day_10;
 mod day_11;
 mod day_12;
 mod day_13;
+mod day_14;
 mod day_1a;
 mod day_1b;
 mod day_2a;
@@ -48,6 +49,8 @@ const CMDS: &[(&str, fn(&mut dyn Read))] = &[
     ("day-12-dot", day_12::run_to_dot),
     ("day-13a", day_13::run_part1),
     ("day-13b", day_13::run_part2),
+    ("day-14a", day_14::run_part1),
+    ("day-14b", day_14::run_part2),
 ];
 
 fn find_cmd(name: &str) -> Option<fn(&mut dyn Read)> {
